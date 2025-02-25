fab373fd6e9467678a51091ac4448e9cbd67a4ac
Aiman Fariz <99475959+AimanFariz@users.noreply.github.com>
2025-02-25 12:51:46 -0600

    remove plotly from home

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 import time
 from about import about_page
 # from compare import compare_page
